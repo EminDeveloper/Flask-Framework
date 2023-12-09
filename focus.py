9ea2fe8a1f744922d30e37c3edda1674ab546812
Emin <emin.f.mammadov@gmail.com>
2023-12-09 16:27:00 +0400

    add first project in Flask Framework

--- focus.py
+++ focus.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Good day and good coding, Flask Framework!</p>"
+    return "<p>Good day and good coding, Flask Framework! Focus you mine</p>"
